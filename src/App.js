26851f5d691da27b9c2aa240d5c8fca9e670e347
Valentina-Maraio <valentina.maraio89@gmail.com>
2022-08-18 14:48:50 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Polpetta Atomica</h1>
-        <div className='text'>
+        <div>
         <ExampleProvider>
           <Example />
         </ExampleProvider>
